0aa5b0d021135ac35dd270deffc44aed82228cf5
Yaloha <ivan.chebotar1995@gmail.com>
2025-11-14 22:10:51 +0300

    log is now locked in place, readme for zip/unzip/tar/untar and grep is written

--- src/log.py
+++ src/log.py
@@ -1,26 +1,39 @@
+import os.path
 from datetime import datetime
+
+
+current_dir = os.path.abspath(__file__)
+temp_dir = os.path.join(current_dir, "..", "temp")
+log_file = os.path.join(temp_dir, "shell.log")
+
+def ensure_log_dir() -> None:
+    """Создает директорию для логов если её нет"""
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir, exist_ok=True)
 
 
 def log_err(cm: str, err: str) -> None:
     """
     logging of errors
     """
-    with open('shell.log', 'a') as f:
+    with open(log_file, 'a') as f:
         f.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] ERROR: {err}\n')
         return
+
 
 def log_success(cm:str) -> None:
     """
     logging of successful executions
     """
-    with open('shell.log', 'a') as f:
+    with open(log_file, 'a') as f:
         f.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] command {cm} was successfully executed\n')
         return
+
 
 def log_command(cm:str) -> None:
     """
     logging of starting command executions
     """
-    with open('shell.log', 'a') as f:
+    with open(log_file, 'a') as f:
         f.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {cm}\n')
         return
